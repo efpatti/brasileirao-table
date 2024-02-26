c7f2a231c1f10eab2acbbb739de11c2beb5b9ad4
efpatti <enzo.patti@aluno.senai.br>
2024-02-26 10:56:44 -0300

    adicionei a opção de outras tabelas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import axios from "axios";
 import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [selectedYear, setSelectedYear] = useState(null);
   const [standings, setStandings] = useState([]);
 
-  const handleClick = async () => {
+  const handleYearChange = async (event) => {
+    const year = event.target.value;
+    setSelectedYear(year);
+    try {
+      const responseData = await fetchStandings(year);
+      setStandings(responseData.data.response[0].league.standings[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchStandings = async (year) => {
     const options = {
       method: "GET",
       url: "https://api-football-beta.p.rapidapi.com/standings",
       params: {
-        season: "2023",
+        season: year,
         league: "71",
       },
       headers: {
@@ -18,41 +30,47 @@ function App() {
       },
     };
 
-    try {
-      const responseData = await axios.request(options);
-      setStandings(responseData.data.response[0].league.standings[0]);
-    } catch (error) {
-      console.error(error);
-    }
+    return await axios.request(options);
   };
 
   return (
     <div>
       <div className="text-center mb-5 mt-5">
-        <button
-          onClick={handleClick}
+        <select
+          value={selectedYear}
+          onChange={handleYearChange}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-          Carregar Standings 2023
-        </button>
+          <option value="">Selecione o Ano</option>
+          {[
+            2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020,
+            2021, 2022, 2023,
+          ].map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
       </div>
 
       {standings && (
         <div className="mt-4">
           {Array.isArray(standings) && standings.length > 0 ? (
             <>
-              <h1 className="text-center font-bold text-lg">Tabela 2023</h1>
+              <h1 className="text-center font-bold text-4xl">
+                Tabela {selectedYear}
+              </h1>
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-50">
                   <tr>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Position
+                      Posição
                     </th>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Team
+                      Time
                     </th>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Points
+                      Pontos
                     </th>
                   </tr>
                 </thead>
