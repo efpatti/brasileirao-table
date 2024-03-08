f9d8a7a600e6c9e3ac26ee6c8ea0d822a529ab10
efpatti <enzo.patti@aluno.senai.br>
2024-03-08 11:54:11 -0300

    project added to portfolio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Tabs, Tab } from './components/Tabs';
-import Brasileiro from "./tabs/Brasileiro"
-import Paulista from './tabs/Paulista';
-const App = () => {
-  return (
-    <div>
-      <Tabs>
-        <Tab label="Brasileirão">
-          <Brasileiro />
-        </Tab>
-        <Tab label="Paulistão">
-          <Paulista />
-        </Tab>
-=======
 import Brasileirao from "./leagues/Brasileirao";
-// import CopaDoBrasil from "./leagues/CopaDoBrasil";
 import Libertadores from "./leagues/Libertadores";
 import Paulista from "./leagues/Paulista";
 import { Tabs, Tab } from "./Tabs";
@@ -42,11 +25,9 @@ const App = () => {
       <Tabs>
         {leaguesData.map((item, i) => (
           <Tab key={i} label={item.label}>
-            {/* Passe a label como propriedade */}
             {React.createElement(item.name)}
           </Tab>
         ))}
->>>>>>> f275342 (project added to portfolio)
       </Tabs>
     </div>
   );
